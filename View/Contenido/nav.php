0e07e162e9a24886ca2db13af520e89c94dcadc9
Jonathan <camanojhonatan@gmail.com>
2020-10-21 11:34:28 -0500

    d

--- View/Contenido/nav.php
+++ View/Contenido/nav.php
@@ -1,5 +1,5 @@
 <div class="bg-light border-right" id="sidebar-wrapper">
-    <div class="sidebar-heading">Coopidrogas </div>
+    <div class="sidebar-heading">Producto PHP</div>
     <div class="list-group list-group-flush">
         <a href="<?php echo SERVERURL; ?>listarUsuario/" class="list-group-item list-group-item-action bg-light">Administrador</a>
         <div class="collapse" id="collapseExample">
